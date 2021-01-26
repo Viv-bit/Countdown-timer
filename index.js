a107bc377fba8dd89bf117e30aece07987a3908c
Jimoh Vivian <jimohvivian@Zainabs-MacBook-Pro-2.local>
2021-01-26 12:15:04 +0100

    completed functionalities

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
-const endCountDown = new Date("Feburary 14, 2021 12:00:05").getTime();
+var endCountDown = new Date("Feb 14, 2021 12:00:05").getTime();
 
 var countDown = setInterval(function() {
     var now = new Date().getTime();
 
     var timeLeft = endCountDown - now;
+
+    var days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var mins = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+    var secs = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+    document.getElementById("days").innerHTML = days 
+    document.getElementById("hours").innerHTML = hours
+    document.getElementById("mins").innerHTML = mins
+    document.getElementById("secs").innerHTML = secs
+
+    if (timeLeft < 0) {
+        clearInterval(countDown);
+        document.getElementById("days").innerHTML = ""
+        document.getElementById("hours").innerHTML = "" 
+        document.getElementById("mins").innerHTML = ""
+        document.getElementById("secs").innerHTML = ""
+        document.getElementById("end").innerHTML = "LAUNCH DAY!!";
+    }
 }, 1000)
